cf7c146d3f68463042233a7eba3c41ee9952e50d
sckang1 <sckang@laonpeople.com>
2024-07-16 17:28:22 +0900

    init21

--- web/assets/packages/flutter_inappwebview/assets/web/web_support.js
+++ web/assets/packages/flutter_inappwebview/assets/web/web_support.js
@@ -485,15 +485,6 @@ window.flutter_inappwebview = {
                 }
                 return null;
             },
-            getContentWidth: function() {
-                var iframe = webView.iframe;
-                try {
-                    return iframe.contentDocument.documentElement.scrollWidth;
-                } catch (e) {
-                    console.log(e);
-                }
-                return null;
-            },
             getSelectedText: function() {
                 var iframe = webView.iframe;
                 try {
